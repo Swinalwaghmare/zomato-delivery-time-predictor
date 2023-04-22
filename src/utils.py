0b1ff24d4644292fa48389596872bdefb16bcdaa
Swinalwaghmare <swinalwaghmare28@gmail.com>
2023-04-23 01:54:14 +0530

    save object created

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pickle
+import numpy as np
+import pandas as pd
+
+from src.exception import ZomatoException
+from src.logger import logging
+
+def save_obeject(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        
+        os.makedirs(dir_path,exist_ok=True)
+        
+        with open(file_path,'wb') as file_obj:
+            pickle.dump(obj,file_obj)
+            
+    except Exception as e:
+        logging.info('Exception occured while saving an object')
+        raise ZomatoException(e,sys)
